02e2af96275c10d1d49b146d88d9cec20ea60da4
FabL-B <errebelneibaf@gmail.com>
2024-11-05 12:42:24 +0100

    Added serializers and viewsets for project app models

--- softdesk_support/project/serializers.py
+++ softdesk_support/project/serializers.py
@@ -1,1 +1,34 @@
 from rest_framework import serializers
+from .models import Project, Contributor, Issue, Comments
+
+
+class ContributorSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Contributor
+        fields = ['id', 'user']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ['id', 'author', 'name', 'description', 'type', 'created_time',]
+
+
+class ContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = ['id', 'user', 'project', 'role']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'author', 'project', 'title', 'description', 'assigned_to', 'priority', 'tag', 'status', 'created_time']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['id', 'author', 'issue', 'description', 'created_time']
