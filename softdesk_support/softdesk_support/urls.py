02106d886e733238ab7cf9dd16b24f729e6ff513
FabL-B <errebelneibaf@gmail.com>
2024-11-12 17:49:36 +0100

    added urls of new apps

--- softdesk_support/softdesk_support/urls.py
+++ softdesk_support/softdesk_support/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('users/', include('users.urls')),
-    path('project/', include('project.urls')),
+    path('projects/', include('projects.urls')),
+    path('contributors/', include('contributors.urls')),
+    path('issues/', include('issues.urls')),
+    path('comments/', include('comments.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
