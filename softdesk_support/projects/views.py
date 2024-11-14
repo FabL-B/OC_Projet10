452e0c03294ad1889ed6d04c7acc3d9c00eebd8e
FabL-B <errebelneibaf@gmail.com>
2024-11-14 18:04:05 +0100

    updated permission_classes call

--- softdesk_support/projects/views.py
+++ softdesk_support/projects/views.py
@@ -1,23 +1,16 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from softdesk_support.permissions import ProjectPermission
 from .models import Project
 from contributors.models import Contributor
 from .serializers import ProjectListSerializer, ProjectDetailSerializer
-from softdesk_support.permissions import IsAuthenticated, IsContributor, IsAuthor
 
 
 class ProjectListViewSet(viewsets.ModelViewSet):
     """Handle listing of all projects."""
     queryset = Project.objects.all()
     serializer_class = ProjectListSerializer
-
-    def get_permissions(self):
-        if self.action == 'list':
-            return [AllowAny()]
-        elif self.action == 'create':
-            return [IsAuthenticated()]
-        return super().get_permissions()   
+    permission_classes = [ProjectPermission]
 
     def perform_create(self, serializer):
         project = serializer.save(author=self.request.user)
@@ -33,10 +26,4 @@ class ProjectDetailViewSet(viewsets.ModelViewSet):
     """Handle CRUD on a specified project"""
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
-
-    def get_permissions(self):
-        if self.action in ['update', 'partial_update', 'destroy']:
-            return [IsAuthor()]
-        elif self.action == 'retrieve':
-            return [IsAuthenticated()]
-        return [IsAuthenticated()]
+    permission_classes = [ProjectPermission]
