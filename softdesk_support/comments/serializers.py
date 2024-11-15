61cb866049638a09ee87de8d23d56a9ed9a00d38
FabL-B <errebelneibaf@gmail.com>
2024-11-15 11:49:17 +0100

    added issue url in comments serializer

--- softdesk_support/comments/serializers.py
+++ softdesk_support/comments/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
 from .models import Comment
 from issues.models import Issue
 
 class CommentSerializer(serializers.ModelSerializer):
     """Serializer for Comment model."""
+    issue_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
         fields = ['id', 'author', 'issue', 'issue_url', 'description', 'created_time']
         read_only_fields = ['id', 'author', 'issue', 'issue_url', 'created_time']
+
+    def get_issue_url(self, obj):
+        request = self.context.get('request')
+        if obj.issue:
+            return reverse(
+                'issues-detail',
+                kwargs={
+                    'project_pk': obj.issue.project.id,
+                    'pk': obj.issue.id
+                },
+                request=request
+            )
+        return None
 
     def validate_issue(self, value):
         project_pk = self.context['view'].kwargs.get('project_pk')
